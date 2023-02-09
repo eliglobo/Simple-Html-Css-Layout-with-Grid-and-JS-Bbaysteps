5d823fc5810deafc9c84d48d950e7d2e83f17eec
Elisa Seeger <eliglobo@gmail.com>
2023-02-09 10:35:37 +0100

    fresh new build - html, cc holygrail layou

---
+++ backend/script.js
@@ -0,0 +1,23 @@
+let name = "Tom"; //String
+let age = 24 //Integer
+let currecny = "$" // Char
+let isInClass = true //Boolean
+let classProgress = 93.2 //float
+let Students = ["Tom", "Danil", "Sandra", "Dotun", "Jenny", "Francisco", "Damir", "Elisa", "Victor"]
+
+function helloJS() {
+    console.log("Hello World from: " + Students[8]);
+    // alert("I LOVE CODE <3 <3 <3")
+}
+
+function RandomColor() {
+    document.querySelector(".Content").style.color = generateHex();
+}
+
+function generateHex() {
+    return  "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+let todoInputList =document.query<selector());
+submit Todo Btn=;
+let todoUI =;
